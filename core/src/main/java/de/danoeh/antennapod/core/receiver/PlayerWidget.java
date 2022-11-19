ccb8a057516815b4851d35d80f7d79878d9b95d2
ByteHamster <info@bytehamster.com>
2022-11-19 16:15:57 +0100

    Remove sync

--- core/src/main/java/de/danoeh/antennapod/core/receiver/PlayerWidget.java
+++ core/src/main/java/de/danoeh/antennapod/core/receiver/PlayerWidget.java
@@ -2,17 +2,8 @@ package de.danoeh.antennapod.core.receiver;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
-import androidx.work.ExistingWorkPolicy;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkManager;
-import de.danoeh.antennapod.core.widget.WidgetUpdaterWorker;
-
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
 public class PlayerWidget extends AppWidgetProvider {
     private static final String TAG = "PlayerWidget";
@@ -31,69 +22,22 @@ public class PlayerWidget extends AppWidgetProvider {
     public void onEnabled(Context context) {
         super.onEnabled(context);
         Log.d(TAG, "Widget enabled");
-        setEnabled(context, true);
-        WidgetUpdaterWorker.enqueueWork(context);
-        scheduleWorkaround(context);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.d(TAG, "onUpdate() called with: " + "context = [" + context + "], appWidgetManager = ["
-                + appWidgetManager + "], appWidgetIds = [" + Arrays.toString(appWidgetIds) + "]");
-        WidgetUpdaterWorker.enqueueWork(context);
 
-        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        if (!prefs.getBoolean(KEY_WORKAROUND_ENABLED, false)) {
-            scheduleWorkaround(context);
-            prefs.edit().putBoolean(KEY_WORKAROUND_ENABLED, true).apply();
-        }
     }
 
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
-        Log.d(TAG, "Widget disabled");
-        setEnabled(context, false);
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
-        Log.d(TAG, "OnDeleted");
-        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        for (int appWidgetId : appWidgetIds) {
-            prefs.edit().remove(KEY_WIDGET_COLOR + appWidgetId).apply();
-            prefs.edit().remove(KEY_WIDGET_PLAYBACK_SPEED + appWidgetId).apply();
-            prefs.edit().remove(KEY_WIDGET_REWIND + appWidgetId).apply();
-            prefs.edit().remove(KEY_WIDGET_FAST_FORWARD + appWidgetId).apply();
-            prefs.edit().remove(KEY_WIDGET_SKIP + appWidgetId).apply();
-        }
-        AppWidgetManager manager = AppWidgetManager.getInstance(context);
-        int[] widgetIds = manager.getAppWidgetIds(new ComponentName(context, PlayerWidget.class));
-        if (widgetIds.length == 0) {
-            prefs.edit().putBoolean(KEY_WORKAROUND_ENABLED, false).apply();
-            WorkManager.getInstance(context).cancelUniqueWork(WORKAROUND_WORK_NAME);
-        }
+
         super.onDeleted(context, appWidgetIds);
     }
 
-    private static void scheduleWorkaround(Context context) {
-        // Enqueueing work enables a BOOT_COMPLETED receiver, which in turn makes Android refresh widgets.
-        // This creates an endless loop with a flickering widget.
-        // Workaround: When there is a widget, schedule a dummy task in the far future, so that the receiver stays.
-        final OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(WidgetUpdaterWorker.class)
-                .setInitialDelay(100 * 356, TimeUnit.DAYS)
-                .build();
-        WorkManager.getInstance(context)
-                .enqueueUniqueWork(WORKAROUND_WORK_NAME, ExistingWorkPolicy.REPLACE, workRequest);
-    }
-
-    public static boolean isEnabled(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        return prefs.getBoolean(KEY_ENABLED, false);
-    }
-
-    private void setEnabled(Context context, boolean enabled) {
-        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        prefs.edit().putBoolean(KEY_ENABLED, enabled).apply();
-    }
 }
