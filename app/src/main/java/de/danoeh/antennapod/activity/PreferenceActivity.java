045cf50ccbb45d5e3073127d373edc301b638dca
ByteHamster <info@bytehamster.com>
2022-11-20 12:58:33 +0100

    Remove feed animatable

--- app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -3,14 +3,13 @@ package de.danoeh.antennapod.activity;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 import de.danoeh.antennapod.R;
 
 public class PreferenceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(android.R.id.content).setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
+        findViewById(android.R.id.content).setBackgroundResource(R.drawable.ic_feed);
         Toast.makeText(this, R.string.this_string_is_used, Toast.LENGTH_SHORT).show();
     }
 }
