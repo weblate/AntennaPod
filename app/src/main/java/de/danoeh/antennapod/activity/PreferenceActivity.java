196f8e67249a99c6e5e2d61e731f650d6cf4edfc
ByteHamster <info@bytehamster.com>
2022-11-20 12:49:52 +0100

    Remove dimen

--- app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -11,8 +11,6 @@ public class PreferenceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         findViewById(android.R.id.content).setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
-        int horizontalSpacing = (int) getResources().getDimension(R.dimen.additional_horizontal_spacing);
-        findViewById(android.R.id.content).setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
         Toast.makeText(this, R.string.this_string_is_used, Toast.LENGTH_SHORT).show();
     }
 }
