54fc1bf37fb4d457136e17a5480087c733712776
ByteHamster <info@bytehamster.com>
2022-11-20 12:30:01 +0100

    Remove preferences

--- app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -1,8 +1,8 @@
 package de.danoeh.antennapod.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.databinding.SettingsActivityBinding;
@@ -15,18 +15,10 @@ public class PreferenceActivity extends AppCompatActivity {
         SettingsActivityBinding binding = SettingsActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(binding.settingsContainer.getId(), new SwipePreferencesFragment(), null)
-                .commit();
         binding.getRoot().setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
         int horizontalSpacing = (int) getResources().getDimension(R.dimen.additional_horizontal_spacing);
         binding.getRoot().setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
-    }
 
-    public static class SwipePreferencesFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            addPreferencesFromResource(R.xml.preferences_swipe);
-        }
+        Toast.makeText(this, R.string.this_string_is_used, Toast.LENGTH_SHORT).show();
     }
 }
