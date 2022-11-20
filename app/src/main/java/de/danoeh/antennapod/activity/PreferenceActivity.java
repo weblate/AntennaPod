d52bfe7e1706ed56cc6a4ea1e9045ca8ce4d5ddc
ByteHamster <info@bytehamster.com>
2022-11-20 12:42:26 +0100

    Remove ViewBinding

--- app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -5,19 +5,16 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.databinding.SettingsActivityBinding;
 
 public class PreferenceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
 
-        SettingsActivityBinding binding = SettingsActivityBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        binding.getRoot().setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
+        findViewById(R.id.settingsContainer).setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
         int horizontalSpacing = (int) getResources().getDimension(R.dimen.additional_horizontal_spacing);
-        binding.getRoot().setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
+        findViewById(R.id.settingsContainer).setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
 
         Toast.makeText(this, R.string.this_string_is_used, Toast.LENGTH_SHORT).show();
     }
