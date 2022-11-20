788addb41c5c0fb2fd26ca9372aa2a95776f9e19
ByteHamster <info@bytehamster.com>
2022-11-20 12:10:48 +0100

    Reduce number of classes

--- app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -2,8 +2,10 @@ package de.danoeh.antennapod.activity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
+import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.databinding.SettingsActivityBinding;
-import de.danoeh.antennapod.fragment.preferences.SwipePreferencesFragment;
 
 public class PreferenceActivity extends AppCompatActivity {
     @Override
@@ -16,5 +18,15 @@ public class PreferenceActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(binding.settingsContainer.getId(), new SwipePreferencesFragment(), null)
                 .commit();
+        binding.getRoot().setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
+        int horizontalSpacing = (int) getResources().getDimension(R.dimen.additional_horizontal_spacing);
+        binding.getRoot().setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
+    }
+
+    public static class SwipePreferencesFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.preferences_swipe);
+        }
     }
 }
