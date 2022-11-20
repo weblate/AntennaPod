762155b13432e0b0981b565ab0a5a40a21f01b8b
ByteHamster <info@bytehamster.com>
2022-11-20 12:48:14 +0100

    Remove layout

--- app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -10,12 +10,9 @@ public class PreferenceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
-
-        findViewById(R.id.settingsContainer).setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
+        findViewById(android.R.id.content).setBackground(AnimatedVectorDrawableCompat.create(this, R.drawable.ic_animate_pause_play));
         int horizontalSpacing = (int) getResources().getDimension(R.dimen.additional_horizontal_spacing);
-        findViewById(R.id.settingsContainer).setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
-
+        findViewById(android.R.id.content).setPadding(horizontalSpacing, 0, horizontalSpacing, 0);
         Toast.makeText(this, R.string.this_string_is_used, Toast.LENGTH_SHORT).show();
     }
 }
